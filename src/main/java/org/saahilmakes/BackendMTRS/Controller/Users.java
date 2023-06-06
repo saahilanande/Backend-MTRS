a05dde603758bea2d61306f1f4d5e3510211ca41
saahilanande <saahilanande21@gmail.com>
2023-06-05 21:02:43 -0500

    Add a new user endpoint added

--- src/main/java/org/saahilmakes/BackendMTRS/Controller/Users.java
+++ src/main/java/org/saahilmakes/BackendMTRS/Controller/Users.java
@@ -2,9 +2,7 @@ package org.saahilmakes.BackendMTRS.Controller;
 
 import org.saahilmakes.BackendMTRS.Model.UserModel;
 import org.saahilmakes.BackendMTRS.Service.UserService;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -23,6 +21,12 @@ public class Users {
 
         List<UserModel> allUser = userService.GetAllUser();
         return allUser;
+    }
+
+    @PostMapping("/adduser")
+    public String add(@RequestBody UserModel userModel){
+       String response = userService.AddNewUser(userModel);
+       return response;
     }
 
 }
--- src/main/java/org/saahilmakes/BackendMTRS/Service/UserService.java
+++ src/main/java/org/saahilmakes/BackendMTRS/Service/UserService.java
@@ -1,5 +1,4 @@
 package org.saahilmakes.BackendMTRS.Service;
-
 import org.saahilmakes.BackendMTRS.Model.UserModel;
 import org.saahilmakes.BackendMTRS.Repository.UsersRepo;
 import org.springframework.stereotype.Service;
@@ -17,5 +16,22 @@ public class UserService {
     public List<UserModel> GetAllUser(){
         List<UserModel> allUser = usersRepo.findAll();
         return  allUser;
+    }
+
+    public String AddNewUser(UserModel userModel){
+
+        UserModel newUser = new UserModel();
+        newUser.setUsername(userModel.getUsername());
+        newUser.setEmail(userModel.getEmail());
+        newUser.setPassword(userModel.getPassword());
+        newUser.setRole(userModel.getRole());
+        newUser.setPhone(userModel.getPhone());
+        try {
+            usersRepo.save(newUser);
+        }
+        catch (Exception ex){
+            return ""+ex;
+        }
+        return "User Added Succesfully";
     }
 }
