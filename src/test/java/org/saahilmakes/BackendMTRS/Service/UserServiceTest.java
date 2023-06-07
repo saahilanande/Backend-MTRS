4e1987074778cbc44a6c5e7fa2903dd2723ce4d3
saahilanande <saahilanande21@gmail.com>
2023-06-07 16:56:53 -0500

    unit test for get all userService added

---
+++ src/test/java/org/saahilmakes/BackendMTRS/Service/UserServiceTest.java
@@ -0,0 +1,47 @@
+package org.saahilmakes.BackendMTRS.Service;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Disabled;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+import org.saahilmakes.BackendMTRS.Repository.UsersRepo;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.verify;
+
+@ExtendWith(MockitoExtension.class)
+@AutoConfigureMockMvc
+class UserServiceTest {
+
+    @Mock
+    private UsersRepo usersRepo;
+
+    private UserService userService;
+
+    @BeforeEach
+    void setUp() {
+        userService = new UserService(usersRepo);
+    }
+
+    @Test
+    void canGetAllUser() {
+        // When
+        userService.GetAllUser();
+
+        // Then
+        verify(usersRepo).findAll();
+    }
+
+    @Test
+    @Disabled
+    void addNewUser() {
+    }
+
+    @Test
+    @Disabled
+    void deleteUser() {
+    }
+}
