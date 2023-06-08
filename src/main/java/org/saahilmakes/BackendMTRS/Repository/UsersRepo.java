99a1a0ec6c8f54624021323f400ba6dfc0a9e43e
saahilanande <saahilanande21@gmail.com>
2023-06-08 18:22:30 -0500

    Unit tests added for emailcheck added

--- src/main/java/org/saahilmakes/BackendMTRS/Repository/UsersRepo.java
+++ src/main/java/org/saahilmakes/BackendMTRS/Repository/UsersRepo.java
@@ -11,7 +11,7 @@ import java.util.List;
 public interface UsersRepo extends JpaRepository<UserModel, Long> {
 
     //Check for email
-    @Query(value = "select case when count(u) > 0 then true else false end from users u where email = :email", nativeQuery = true)
+    @Query(value = "select case when count(*) > 0 then true else false end from users where email = :email", nativeQuery = true)
     boolean findEmail(String email);
 
 }
---
+++ src/test/java/org/saahilmakes/BackendMTRS/Repository/UsersRepoTest.java
@@ -0,0 +1,62 @@
+package org.saahilmakes.BackendMTRS.Repository;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.Test;
+import org.saahilmakes.BackendMTRS.Model.UserModel;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+
+
+@DataJpaTest
+class UsersRepoTest {
+
+    @Autowired
+    private UsersRepo usersRepo;
+
+    @AfterEach
+    void tearDown() {
+        usersRepo.deleteAll();
+    }
+
+    @Test
+    void itShouldCheckIfEmailExist() {
+
+        //Given
+        UserModel newUser = new UserModel();
+        newUser.setEmail("test@test.com");
+        newUser.setPhone(9999);
+        newUser.setPassword("test");
+        newUser.setRole("test");
+        newUser.setUsername("test");
+        usersRepo.save(newUser);
+        String emailToCheck = "test@test.com";
+
+        //When
+        Boolean expected = usersRepo.findEmail(emailToCheck);
+
+        //Then
+        assertThat(expected).isTrue();
+    }
+
+    @Test
+    void itShouldCheckIfNotEmailExist() {
+
+        //Given
+        UserModel newUser = new UserModel();
+        newUser.setEmail("test@test.com");
+        newUser.setPhone(9999);
+        newUser.setPassword("test");
+        newUser.setRole("test");
+        newUser.setUsername("test");
+        usersRepo.save(newUser);
+        String emailToCheck = "email@.com";
+
+        //When
+        Boolean expected = usersRepo.findEmail(emailToCheck);
+
+        //Then
+        assertThat(expected).isFalse();
+    }
+}
