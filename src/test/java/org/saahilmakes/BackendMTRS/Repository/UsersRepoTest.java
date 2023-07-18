2eef5252c8945c0520e954d7ddbf682ad003e60d
saahilanande <saahilanande21@gmail.com>
2023-07-17 23:24:23 -0500

    Prod build

--- src/test/java/org/saahilmakes/BackendMTRS/Repository/UsersRepoTest.java
+++ src/test/java/org/saahilmakes/BackendMTRS/Repository/UsersRepoTest.java
@@ -1,82 +1,82 @@
-package org.saahilmakes.BackendMTRS.Repository;
-
-import org.junit.jupiter.api.AfterEach;
-import org.junit.jupiter.api.Test;
-import org.saahilmakes.BackendMTRS.Model.UserModel;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
-
-import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
-
-
-@DataJpaTest
-class UsersRepoTest {
-
-    @Autowired
-    private UsersRepo usersRepo;
-
-    @AfterEach
-    void tearDown() {
-        usersRepo.deleteAll();
-    }
-
-//    @Test
-//    void itShouldCheckIfEmailExist() {
+//package org.saahilmakes.BackendMTRS.Repository;
 //
-//        //Given
-//        UserModel newUser = new UserModel();
-//        newUser.setEmail("test@test.com");
-//        newUser.setPhone(9999);
-//        newUser.setPassword("test");
-//        newUser.setRole("test");
-//        newUser.setUsername("test");
-//        usersRepo.save(newUser);
-//        String emailToCheck = "test@test.com";
+//import org.junit.jupiter.api.AfterEach;
+//import org.junit.jupiter.api.Test;
+//import org.saahilmakes.BackendMTRS.Model.UserModel;
+//import org.springframework.beans.factory.annotation.Autowired;
+//import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 //
-//        //When
-//        Boolean expected = usersRepo.findEmail(emailToCheck);
+//import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 //
-//        //Then
-//        assertThat(expected).isTrue();
+//
+//@DataJpaTest
+//class UsersRepoTest {
+//
+//    @Autowired
+//    private UsersRepo usersRepo;
+//
+//    @AfterEach
+//    void tearDown() {
+//        usersRepo.deleteAll();
 //    }
 //
-//    @Test
-//    void itShouldCheckIfNotEmailExist() {
-//
-//        //Given
-//        UserModel newUser = new UserModel();
-//        newUser.setEmail("test@test.com");
-//        newUser.setPhone(9999);
-//        newUser.setPassword("test");
-//        newUser.setRole("test");
-//        newUser.setUsername("test");
-//        usersRepo.save(newUser);
-//        String emailToCheck = "email@.com";
-//
-//        //When
-//        Boolean expected = usersRepo.findEmail(emailToCheck);
-//
-//        //Then
-//        assertThat(expected).isFalse();
-//    }
-//
-//    @Test
-//    void itShouldGetUserDetailsByEmail() {
-//
-//        //Given
-//        UserModel newUser = new UserModel();
-//        newUser.setEmail("test@test.com");
-//        newUser.setPhone(9999);
-//        newUser.setPassword("test");
-//        newUser.setRole("test");
-//        newUser.setUsername("test");
-//        usersRepo.save(newUser);
-//        String emailToCheck = "test@test.com";
-//
-//        //When
-//        UserModel user = usersRepo.getUserDetailsbyEmail(emailToCheck);
-//
-//        //Then
-//        assertThat(user).isEqualTo(newUser);
-//    }
-}
+////    @Test
+////    void itShouldCheckIfEmailExist() {
+////
+////        //Given
+////        UserModel newUser = new UserModel();
+////        newUser.setEmail("test@test.com");
+////        newUser.setPhone(9999);
+////        newUser.setPassword("test");
+////        newUser.setRole("test");
+////        newUser.setUsername("test");
+////        usersRepo.save(newUser);
+////        String emailToCheck = "test@test.com";
+////
+////        //When
+////        Boolean expected = usersRepo.findEmail(emailToCheck);
+////
+////        //Then
+////        assertThat(expected).isTrue();
+////    }
+////
+////    @Test
+////    void itShouldCheckIfNotEmailExist() {
+////
+////        //Given
+////        UserModel newUser = new UserModel();
+////        newUser.setEmail("test@test.com");
+////        newUser.setPhone(9999);
+////        newUser.setPassword("test");
+////        newUser.setRole("test");
+////        newUser.setUsername("test");
+////        usersRepo.save(newUser);
+////        String emailToCheck = "email@.com";
+////
+////        //When
+////        Boolean expected = usersRepo.findEmail(emailToCheck);
+////
+////        //Then
+////        assertThat(expected).isFalse();
+////    }
+////
+////    @Test
+////    void itShouldGetUserDetailsByEmail() {
+////
+////        //Given
+////        UserModel newUser = new UserModel();
+////        newUser.setEmail("test@test.com");
+////        newUser.setPhone(9999);
+////        newUser.setPassword("test");
+////        newUser.setRole("test");
+////        newUser.setUsername("test");
+////        usersRepo.save(newUser);
+////        String emailToCheck = "test@test.com";
+////
+////        //When
+////        UserModel user = usersRepo.getUserDetailsbyEmail(emailToCheck);
+////
+////        //Then
+////        assertThat(user).isEqualTo(newUser);
+////    }
+//}
--- src/test/java/org/saahilmakes/BackendMTRS/Service/UserServiceTest.java
+++ src/test/java/org/saahilmakes/BackendMTRS/Service/UserServiceTest.java
@@ -1,120 +1,120 @@
-package org.saahilmakes.BackendMTRS.Service;
-
-import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.Disabled;
-import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.extension.ExtendWith;
-import org.mockito.ArgumentCaptor;
-import org.mockito.Mock;
-import org.mockito.junit.jupiter.MockitoExtension;
-import org.saahilmakes.BackendMTRS.Model.UserModel;
-import org.saahilmakes.BackendMTRS.Repository.UsersRepo;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
-import org.springframework.security.authentication.AuthenticationManager;
-
-import java.util.Map;
-
-import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
-import static org.mockito.Mockito.verify;
-
-@ExtendWith(MockitoExtension.class)
-@AutoConfigureMockMvc
-class UserServiceTest {
-
-    @Mock
-    private UsersRepo usersRepo;
-
-    private UserService userService;
-
-    @Mock
-    AuthenticationManager authenticationManager;
-
-    @Mock
-    TokenService tokenService;
-
-    @BeforeEach
-    void setUp() {
-        userService = new UserService(usersRepo, authenticationManager, tokenService);
-    }
-
-//    @Test
-//    void canGetAllUser() {
-//        // When
-//        userService.GetAllUser();
+//package org.saahilmakes.BackendMTRS.Service;
 //
-//        // Then
-//        verify(usersRepo).findAll();
+//import org.junit.jupiter.api.BeforeEach;
+//import org.junit.jupiter.api.Disabled;
+//import org.junit.jupiter.api.Test;
+//import org.junit.jupiter.api.extension.ExtendWith;
+//import org.mockito.ArgumentCaptor;
+//import org.mockito.Mock;
+//import org.mockito.junit.jupiter.MockitoExtension;
+//import org.saahilmakes.BackendMTRS.Model.UserModel;
+//import org.saahilmakes.BackendMTRS.Repository.UsersRepo;
+//import org.springframework.beans.factory.annotation.Autowired;
+//import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+//import org.springframework.security.authentication.AuthenticationManager;
+//
+//import java.util.Map;
+//
+//import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+//import static org.mockito.Mockito.verify;
+//
+//@ExtendWith(MockitoExtension.class)
+//@AutoConfigureMockMvc
+//class UserServiceTest {
+//
+//    @Mock
+//    private UsersRepo usersRepo;
+//
+//    private UserService userService;
+//
+//    @Mock
+//    AuthenticationManager authenticationManager;
+//
+//    @Mock
+//    TokenService tokenService;
+//
+//    @BeforeEach
+//    void setUp() {
+//        userService = new UserService(usersRepo, authenticationManager, tokenService);
 //    }
-
-//    @Test
-//    void addNewUser() {
-//        //Given
-//        UserModel newUser = new UserModel();
-//        newUser.setEmail("test@test.com");
-//        newUser.setPhone(9999);
-//        newUser.setPassword("test");
-//        newUser.setRole("test");
-//        newUser.setUsername("test");
 //
-//        //When
-//        userService.AddNewUser(newUser);
+////    @Test
+////    void canGetAllUser() {
+////        // When
+////        userService.GetAllUser();
+////
+////        // Then
+////        verify(usersRepo).findAll();
+////    }
 //
-//        //Then
-//        ArgumentCaptor<UserModel> userModelArgumentCaptor = ArgumentCaptor.forClass(UserModel.class);
-//        verify(usersRepo).save(userModelArgumentCaptor.capture());
+////    @Test
+////    void addNewUser() {
+////        //Given
+////        UserModel newUser = new UserModel();
+////        newUser.setEmail("test@test.com");
+////        newUser.setPhone(9999);
+////        newUser.setPassword("test");
+////        newUser.setRole("test");
+////        newUser.setUsername("test");
+////
+////        //When
+////        userService.AddNewUser(newUser);
+////
+////        //Then
+////        ArgumentCaptor<UserModel> userModelArgumentCaptor = ArgumentCaptor.forClass(UserModel.class);
+////        verify(usersRepo).save(userModelArgumentCaptor.capture());
+////
+////        UserModel capturedUser = userModelArgumentCaptor.getValue();
+////        assertThat(capturedUser).isEqualTo(newUser);
+////    }
 //
-//        UserModel capturedUser = userModelArgumentCaptor.getValue();
-//        assertThat(capturedUser).isEqualTo(newUser);
-//    }
-
-//    @Test
-//    void canDeleteUser() {
-//        //Given
-//        long id = 2;
-//        //When
-//        userService.DeleteUser(id);
-//        //Then
-//        verify(usersRepo).deleteById(id);
+////    @Test
+////    void canDeleteUser() {
+////        //Given
+////        long id = 2;
+////        //When
+////        userService.DeleteUser(id);
+////        //Then
+////        verify(usersRepo).deleteById(id);
+////
+////    }
 //
-//    }
-
-//    @Test
-//    void UserShouldBeValid() {
-//        //Given
-//        UserModel newUser = new UserModel();
-//        newUser.setEmail("test@test.com");
-//        newUser.setPhone(9999);
-//        newUser.setPassword("test");
-//        newUser.setRole("test");
-//        newUser.setUsername("test");
-//        usersRepo.save(newUser);
-//        String emailToCheck = "test@test.com";
-//        String passwordToCheck ="test";
+////    @Test
+////    void UserShouldBeValid() {
+////        //Given
+////        UserModel newUser = new UserModel();
+////        newUser.setEmail("test@test.com");
+////        newUser.setPhone(9999);
+////        newUser.setPassword("test");
+////        newUser.setRole("test");
+////        newUser.setUsername("test");
+////        usersRepo.save(newUser);
+////        String emailToCheck = "test@test.com";
+////        String passwordToCheck ="test";
+////
+////        //When
+////        Map<String, String> result = userService.ValidateUser(emailToCheck,passwordToCheck);
+////        //Then
+////        assertThat(result.get("message")).isEqualTo("Login successful");
+////    }
 //
-//        //When
-//        Map<String, String> result = userService.ValidateUser(emailToCheck,passwordToCheck);
-//        //Then
-//        assertThat(result.get("message")).isEqualTo("Login successful");
-//    }
-
-//    @Test
-//    @Disabled
-//    void UserShouldNotBeValid() {
-//        //Given
-//        UserModel newUser = new UserModel();
-//        newUser.setEmail("test@test.com");
-//        newUser.setPhone(9999);
-//        newUser.setPassword("test");
-//        newUser.setRole("test");
-//        newUser.setUsername("test");
-//        usersRepo.save(newUser);
-//        String emailToCheck = "test@test";
-//        String passwordToCheck ="wrongpass";
-//
-//        //When
-//        Map<String, String> result = userService.ValidateUser(emailToCheck,passwordToCheck);
-//        //Then
-//        assertThat(result.get("message")).isEqualTo("Invalid");
-//    }
-}
+////    @Test
+////    @Disabled
+////    void UserShouldNotBeValid() {
+////        //Given
+////        UserModel newUser = new UserModel();
+////        newUser.setEmail("test@test.com");
+////        newUser.setPhone(9999);
+////        newUser.setPassword("test");
+////        newUser.setRole("test");
+////        newUser.setUsername("test");
+////        usersRepo.save(newUser);
+////        String emailToCheck = "test@test";
+////        String passwordToCheck ="wrongpass";
+////
+////        //When
+////        Map<String, String> result = userService.ValidateUser(emailToCheck,passwordToCheck);
+////        //Then
+////        assertThat(result.get("message")).isEqualTo("Invalid");
+////    }
+//}
