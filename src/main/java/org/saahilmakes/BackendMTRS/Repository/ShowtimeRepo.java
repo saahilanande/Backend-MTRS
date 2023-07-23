5ef855aeeb188934ec8f95a8f281b49f91d32bad
saahilanande <saahilanande21@gmail.com>
2023-07-23 18:28:48 -0500

    Showtime repository added

---
+++ src/main/java/org/saahilmakes/BackendMTRS/Repository/ShowtimeRepo.java
@@ -0,0 +1,6 @@
+package org.saahilmakes.BackendMTRS.Repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ShowtimeRepo extends JpaRepository<ShowtimeRepo, Long> {
+}
