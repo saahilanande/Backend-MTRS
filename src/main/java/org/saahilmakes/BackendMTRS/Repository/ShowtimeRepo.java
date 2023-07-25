3aeec8386cd1d28f5cd138941d7647ae5bfbeaef
saahilanande <saahilanande21@gmail.com>
2023-07-24 20:18:48 -0500

    Custom query added to fetch showtime for specific movie

--- src/main/java/org/saahilmakes/BackendMTRS/Repository/ShowtimeRepo.java
+++ src/main/java/org/saahilmakes/BackendMTRS/Repository/ShowtimeRepo.java
@@ -1,8 +1,15 @@
 package org.saahilmakes.BackendMTRS.Repository;
 
 import org.saahilmakes.BackendMTRS.Model.ShowtimeModel;
+import org.saahilmakes.BackendMTRS.Service.ShowtimeService;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+import java.util.List;
 
 public interface ShowtimeRepo extends JpaRepository<ShowtimeModel, Long> {
+
+    @Query(value = "select * from showtimes where movieId = :id", nativeQuery = true)
+    List<ShowtimeService> findShowtime(Long id);
 
 }
