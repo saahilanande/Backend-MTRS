ae1d53dce1e9b6083a9e80748f710c682699c1d3
saahilanande <saahilanande21@gmail.com>
2023-07-27 12:27:18 -0500

    Endpoint added to get single movie detail using id

--- src/main/java/org/saahilmakes/BackendMTRS/Controller/Movie.java
+++ src/main/java/org/saahilmakes/BackendMTRS/Controller/Movie.java
@@ -8,6 +8,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
+import java.util.Optional;
 
 @RestController
 @RequestMapping("/movie")
@@ -21,21 +22,27 @@ public class Movie {
 
     //Get all the movies from the database
     @GetMapping("/")
-    public List<MovieModel> getAllMovies(@Min(value = 0) @Max(value = 300) @RequestParam(value = "pageNo", required = false, defaultValue = "0") int pNo,@Min(value = 10) @Max(value = 50) @RequestParam(value = "pageSize", required = false, defaultValue = "10") int pSize) {
+    public List<MovieModel> getAllMovies(@Min(value = 0) @Max(value = 300) @RequestParam(value = "pageNo", required = false, defaultValue = "0") int pNo, @Min(value = 10) @Max(value = 50) @RequestParam(value = "pageSize", required = false, defaultValue = "10") int pSize) {
         List<MovieModel> movies = movieService.getLatestMovies(pNo, pSize);
         return movies;
     }
 
+    //Get movie with ID
+    @GetMapping("/{id}")
+    public Optional<MovieModel> getMovieDetail(@PathVariable Long id) {
+        return movieService.getMovieDetails(id);
+    }
+
     //Add a new movie endpoint
     @PostMapping("/add")
-    public ResponseEntity addNewMovie(@RequestBody MovieModel movieModel){
+    public ResponseEntity addNewMovie(@RequestBody MovieModel movieModel) {
         ResponseEntity response = movieService.addNewMovie(movieModel);
         return response;
     }
 
     //Delete a movie by Id endpoint
     @DeleteMapping("/delete/{id}")
-    public String deleteMovie(@PathVariable("id") Long id){
+    public String deleteMovie(@PathVariable("id") Long id) {
         String response = movieService.deleteMovie(id);
         return response;
     }
