d237e3414b4f389c2e3d1f03ed5b86b9f4dbc139
saahilanande <saahilanande21@gmail.com>
2023-07-28 20:33:08 -0500

    movieshowtime model added

---
+++ src/main/java/org/saahilmakes/BackendMTRS/Model/movieShowtimeModel.java
@@ -0,0 +1,44 @@
+package org.saahilmakes.BackendMTRS.Model;
+
+import com.fasterxml.jackson.annotation.JsonFormat;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
+
+import java.sql.Date;
+
+@Entity
+@Table(name = "movieshowtime")
+public class movieShowtimeModel {
+
+    private Long movieId;
+    private String name;
+    private String address;
+    private String city;
+    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd")
+    private Date movieDate;
+    private String startTimes;
+
+    public Long getMovieId() {
+        return movieId;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    public Date getMovieDate() {
+        return movieDate;
+    }
+
+    public String getStartTimes() {
+        return startTimes;
+    }
+}
